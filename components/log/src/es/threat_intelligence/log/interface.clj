24a3c954119f66a8ed683c358c4da0fb431d9b66
Eric Stewart <eric@eric-stewart.com>
2024-05-03 14:52:21 -0500

    doc: A few basic function docstrings

--- components/log/src/es/threat_intelligence/log/interface.clj
+++ components/log/src/es/threat_intelligence/log/interface.clj
@@ -3,13 +3,16 @@
 
 
 (defn info
+  "A info level log message"
   [message & other-data]
   (core/info message other-data))
 
 (defn debug
+  "A debug level log message"
   [message & other-data]
   (core/debug message other-data))
 
 (defn error
+  "An error level log message"
   [message & other-data]
   (core/error message other-data))
