3656b2398c1c3eee5cff4aa47ce8ca954bf35ed5
Eric Stewart <eric@eric-stewart.com>
2024-05-04 12:17:32 -0500

    fix: Configuration/imports for spinning up Component systems in dev
    Also fixed load paths for the test profile. The poly tool was finding/executing
    tests just fine, but this caused a problem when trying to load everything with
    Component repl tools/helpers while the test profile was includeed.

--- development/src/dev.clj
+++ development/src/dev.clj
@@ -1,5 +1,15 @@
-(ns dev)
+(ns dev
+  (:require [es.threat-intelligence.rest-api.system :as rest-api-system]
+            [com.stuartsierra.component :as component]
+            [com.stuartsierra.component.repl :as crepl :refer [set-init start stop reset system]]))
 
-(require '[com.stuartsierra.component.repl :as crepl])
+#_(def ra-system (rest-api-system/new-system :dev))
+
 (comment
+
+  (reset)
+  (set-init (fn [_old-system] (rest-api-system/new-system :dev)))
+  (start)
+  (stop)
+  (keys system)
   :rcf)
